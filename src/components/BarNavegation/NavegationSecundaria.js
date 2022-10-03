175b70dccff70baf81d1e4fde4bee678ef328b49
FJardim <licdafeliciajardim0305@gmail.com>
2022-10-03 13:36:17 -0400

    banner

--- src/components/BarNavegation/NavegationSecundaria.js
+++ src/components/BarNavegation/NavegationSecundaria.js
@@ -3,15 +3,23 @@ import { FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter, FaYoutube } from "rea
 const NavegationSecundaria = () => {
     return (
         <div className=" bg-main-dark text-black h-14 px-8">
-            <div className="container h-full">
-                <div className="flex justify-end items-center p-2 space-x-1">
+            <div className="container flex justify-around h-full">
+                <div className="flex items-center p-2 space-x-1">
                     <FaFacebook className="hover:text-white" />
                     <FaInstagram className="hover:text-white" />
                     <FaLinkedinIn className="hover:text-white" />
                     <FaTwitter className="hover:text-white" />
                     <FaYoutube className="hover:text-white" />
-                    {/* <div className="border-r-4 border-current "></div> */}
-                    <select className=" bg-main text-slate-600 outline-0" id="">
+                </div>
+                <div className="flex py-2 px-full items-center ">
+                    <span className="pr-8">Ayudas</span>
+                    <span className="pr-8">Sobre Nosotros</span>
+                    <span className="pr-8">Contacto</span>
+                    <span className="pr-8">Vender en Sínoxsa</span>
+                    <span className="pr-8">Tiendas</span>
+                </div>
+                <div className="flex items-center">
+                    <select className=" bg-main-dark border-main-dark text-slate-600 outline-0" id="">
                         <option>English</option>
                         <option>Spanish</option>
                     </select>
--- src/components/BarNavegation/Search.js
+++ src/components/BarNavegation/Search.js
@@ -1,7 +1,7 @@
 const search = () => {
     return (
         <div className="flex p-2 ml-4 ">
-            <select className="px-4 h-10 w-24 rounded-l-lg bg-white outline-0" id="">
+            <select className="px-4 h-10 w-24 rounded-l-lg border-white bg-white outline-0" id="">
                 <option>All</option>
                 <option>Categoria 1</option>
                 <option>Categoria 2</option>
