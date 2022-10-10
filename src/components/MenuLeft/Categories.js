b9c9ff7102d9792390094b0afd84bdf7b52ac1d5
luis <voldelord26@gmail.com>
2022-10-10 12:28:47 -0400

    feature (prodctss) update componets menu left

--- src/components/MenuLeft/Categories.js
+++ src/components/MenuLeft/Categories.js
@@ -2,14 +2,18 @@ import React from "react";
 
 const Categories = () => {
   return (
-    <div className="Container mt-6">
+    <div className="lg:ml-6 bg-white lg:w-64 w-56 m-auto md:w-40 rounded-lg shadow p-4">
       <div>
-        <p className="text-lg font-bold">Categories</p>
+        <h4 className="text-lg font-bold">Categories</h4>
+      </div>
+      <div className="ml-4">
+        <p>Perneria</p>
+        <p>Contruccion</p>
         <p>Electricidad</p>
         <p>Plomeria</p>
-        <p>Pintura</p>
         <p>Cerrajeria</p>
-        <p>Perneria</p>
+        <p>Herramientas</p>
+        <p>Pintura</p>
       </div>
     </div>
   );
